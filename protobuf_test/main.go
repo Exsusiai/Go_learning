cd7b26d0eacc5ff570e85d0ee9cf16ab573651d5
Jingsheng <cjingsheng01@163.com>
2023-05-29 21:14:35 +0200

    modify protobuf serialized function

--- protobuf_test/main.go
+++ protobuf_test/main.go
@@ -6,69 +6,84 @@ import (
 	"log"
 	"time"
 
-	"github.com/Exsusiai/protobuf_test/proto_path"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const(
 	CSV_PATH = "/home/jason/LD_works/Go/test_csv/c3.csv"
 	DB_NAME = "test_cols_csv"
-	COLLETION_NAME = "c3"
-	OUTPUT_FILE_NAME = "page_output_test_2.bin"
-	SEARCH_ID = 10164
+	COLLETION_NAME = "c1"
+	OUTPUT_FILE_NAME = "page_output_test_5.bin"
+	SEARCH_ID = 3734554
+	PROGRAM_RUN_MODE = 3 // 1: csv_To_MongoDB, 2: MongoDB_To_bin, 3: retrieve Message from .bin
 )
 
 func main() {
 	//start the timer
 	start := time.Now()
 
-	// Read the CSV file and write to MongoDB
-	// err := csvToMongo(CSV_PATH, DB_NAME, COLLETION_NAME)
-	// if err != nil {
-	// 		log.Fatalf("Error in csvToMongo: %v", err)
-	// }
+	switch PROGRAM_RUN_MODE {
+		case 1:
+		// STEP 1：Read the CSV file and write to MongoDB
+		err := csvToMongo(CSV_PATH, DB_NAME, COLLETION_NAME)
+		if err != nil {
+				log.Fatalf("Error in csvToMongo: %v", err)
+		}
+		fmt.Println("Data has been successfully written to MongoDB")
 
-	// // Set up MongoDB connection
+		case 2:
+		// STEP 2：Connect to MongoDB
+		ctx := context.Background()
+		client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer client.Disconnect(ctx)
 
-	// ctx := context.Background()
-	// client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer client.Disconnect(ctx)
+		err = client.Ping(ctx, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Connected to MongoDB!")
 
-	// err = client.Ping(ctx, nil)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println("Connected!")
+		// // STEP 3：Read data from MongoDB, serialize, and write to a .pb file
+		// // Read data from MongoDB
+		// pages, err := readDataFromDB(ctx, client)
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
 
-	// // Read data from MongoDB
-	// pages, err := readDataFromDB(ctx, client)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+		// fmt.Printf("Size of 'pages': %d\n", len(pages))
 
-	// fmt.Printf("Size of 'pages': %d\n", len(pages))
+		// // Serialize data
+		// data, err := serializeData(pages)
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
 
-	// // Serialize data
-	// data, err := serializeData(pages)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+		// // Write data to a .proto file
+		// err = writeDataToFile(data, OUTPUT_FILE_NAME, DB_NAME, COLLETION_NAME)
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
 
-	// // Write data to a .proto file
-	// err = writeDataToFile(data, OUTPUT_FILE_NAME)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+		// fmt.Println("Data has been successfully written to a .pb file")
 
-	// fmt.Println("Data has been successfully written to a .pb file")
+		// STEP 3 Version_2：Read data from MongoDB, serialize, and write to a .pb file
+		err = MongoDB_To_bin(ctx, client, DB_NAME, COLLETION_NAME, OUTPUT_FILE_NAME)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Data has been successfully written to a .pb file")
 
-	err := retrieveMessage(SEARCH_ID, OUTPUT_FILE_NAME)
-	if err != nil {
-		log.Fatal(err)
+		case 3:
+		// STEP 4：Read data from a .pb file and search for a specific page
+		err := retrieveMessage(SEARCH_ID, OUTPUT_FILE_NAME)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Retrieved finished")
 	}
 
 	//calculate the elapsed time
@@ -79,25 +94,4 @@ func main() {
 	fmt.Println("---------------------------------")
 }
 
-func readDataFromDB(ctx context.Context, client *mongo.Client) ([]*proto_path.Page, error) {
-	collection := client.Database(DB_NAME).Collection(COLLETION_NAME)
-	cursor, err := collection.Find(ctx, bson.D{})
-	if err != nil {
-		return nil, err
-	}
-	defer cursor.Close(ctx)
 
-	var pages []*proto_path.Page
-
-	for cursor.Next(ctx) {
-		var p *proto_path.Page
-		err := cursor.Decode(&p)
-		if err != nil {
-			log.Fatal(err)
-		}
-		pages = append(pages, p)
-
-	}
-	
-	return pages, nil
-}
--- protobuf_test/proto_funcs.go
+++ protobuf_test/proto_funcs.go
@@ -1,47 +1,122 @@
 package main
 
 import (
-	"encoding/binary"
+	"context"
 	"fmt"
 	"io"
-	"io/ioutil"
+	"log"
 	"os"
 
 	"github.com/Exsusiai/protobuf_test/proto_path"
 	"github.com/golang/protobuf/proto"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-// The function to serialize data
-func serializeData(pages []*proto_path.Page) ([]byte, error) {
-	var data []byte
+// func readDataFromDB(ctx context.Context, client *mongo.Client, _db_name string, _collection_name string ) ([]*proto_path.Page, error) {
+// 	collection := client.Database(_db_name).Collection(_collection_name)
+// 	cursor, err := collection.Find(ctx, bson.D{})
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer cursor.Close(ctx)
+
+// 	var pages []*proto_path.Page
+
+// 	for cursor.Next(ctx) {
+// 		var p *proto_path.Page
+// 		err := cursor.Decode(&p)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		pages = append(pages, p)
+// 	}
+
+// 	return pages, nil
+// }
+
+// // The function to serialize data
+// func serializeData(pages []*proto_path.Page) ([]byte, error) {
+// 	var data []byte
+
+// 	for _, p := range pages {
+// 		// Serialize the page
+// 		serializedPage, err := proto.Marshal(p)
+// 		if err != nil {
+// 			return nil, err
+// 		}
+
+// 		var FrameId uint32 = uint32(p.GetPageId())
+// 		var Timestamp uint64 = 0
+// 		var FrameLength uint32 = uint32(len(serializedPage))
+
+// 		FrameHeader_data := make([]byte, 16)
+
+// 		binary.BigEndian.PutUint32(FrameHeader_data[0:4], FrameId)
+// 		binary.BigEndian.PutUint64(FrameHeader_data[4:12], Timestamp)
+// 		binary.BigEndian.PutUint32(FrameHeader_data[12:16], FrameLength)
+
+// 		// Append the serialized page to the data
+// 		data = append(data, FrameHeader_data...)
+// 		data = append(data, serializedPage...)
+// 	}
+// 	return data, nil
+// }
+
+// // The function to write data to a .proto file
+// func writeDataToFile(data []byte, filename string) error {
+// 	return ioutil.WriteFile(filename, data, 0644)
+// }
+
+func MongoDB_To_bin(ctx context.Context, client *mongo.Client, _db_name string, _collection_name string, filename string) error{
+	file, err := os.Create(filename)
+	if err != nil {
+    log.Fatal(err)
+	}
+	defer file.Close()
 	
-	for _, p := range pages {
-		// Serialize the page
-		serializedPage, err := proto.Marshal(p)
+	collection := client.Database(_db_name).Collection(_collection_name)
+	cursor, err := collection.Find(ctx, bson.D{})
+	if err != nil {
+		return err
+	}
+	defer cursor.Close(ctx)
+
+	for cursor.Next(ctx) {
+		var p *proto_path.Page
+		err := cursor.Decode(&p)
 		if err != nil {
-			return nil, err
+			return err
 		}
 
-		var FrameId uint32 = uint32(p.GetPageId())
-		var Timestamp uint64 = 0
-		var FrameLength uint32 = uint32(len(serializedPage))
+		serializedPage, err := proto.Marshal(p)
+		if err != nil {
+			return err
+		}
 
-		FrameHeader_data := make([]byte, 16)
+		header := &proto_path.FrameHeader{
+			FrameId:     uint32(p.PageId),
+			Timestamp:   0,
+			FrameLength: uint32(len(serializedPage)),
+		}
+		serializedHeader, err := proto.Marshal(header)
+		if err != nil {
+			return err
+		}
 
-		binary.BigEndian.PutUint32(FrameHeader_data[0:4], FrameId)
-		binary.BigEndian.PutUint64(FrameHeader_data[4:12], Timestamp)
-		binary.BigEndian.PutUint32(FrameHeader_data[12:16], FrameLength)		
+		// println(len(serializedHeader))
 
-		// Append the serialized page to the data
-		data = append(data, FrameHeader_data...)
-		data = append(data, serializedPage...)
+		_, err = file.Write(serializedHeader)
+		if err != nil {
+			return err
+		}
+		_, err = file.Write(serializedPage)
+		if err != nil {
+			return err
+		}
 	}
-	return data, nil
-}
 
-// The function to write data to a .proto file
-func writeDataToFile(data []byte, filename string) error {
-	return ioutil.WriteFile(filename, data, 0644)
+	return nil
 }
 
 // The function to read data from a .pb file
@@ -55,8 +130,8 @@ func retrieveMessage(id int, filename string) error {
 
 	for {
 			// read frame header
-			headerBuf := make([]byte, 16) 
-			_, err := file.Read(headerBuf)
+			headerBuf := make([]byte, 10)
+			_, err := file.Read(headerBuf)			
 			if err == io.EOF {
 					fmt.Println("End of file reached")
 					break
@@ -67,19 +142,19 @@ func retrieveMessage(id int, filename string) error {
 					return err
 			}
 
-			frameId := uint32(binary.BigEndian.Uint32(headerBuf[0:4]))
-			// timestamp := uint64(binary.BigEndian.Uint64(headerBuf[4:12]))
-			frameLength := uint32(binary.BigEndian.Uint32(headerBuf[12:16]))
+			var frameHeader proto_path.FrameHeader
+			err = proto.Unmarshal(headerBuf, &frameHeader)
+			if err != nil {
+					fmt.Println("Error unmarshalling header: ", err)
+					return err
+			}
 
-			// fmt.Printf("FrameId: %v\n", frameId)
-			// fmt.Printf("Timestamp: %v\n", timestamp)
-			// fmt.Printf("FrameLength: %v\n", frameLength)
 
 			//check if the message id matches
-			if int(frameId) == id {
+			if int(frameHeader.GetFrameId()) == id {
 					fmt.Println("Frame ID matches.")
 					// if it matches, read the message
-					msgBuf := make([]byte, frameLength) // Allocate buffer according to FrameLength
+					msgBuf := make([]byte, frameHeader.GetFrameLength()) // Allocate buffer according to FrameLength
 					_, err := file.Read(msgBuf)
 					if err != nil {
 							fmt.Println("Error reading file for message: ", err)		
@@ -106,7 +181,7 @@ func retrieveMessage(id int, filename string) error {
 					return nil
 			} else {
 					// if it does not match, skip the message
-					_, err = file.Seek(int64(frameLength), io.SeekCurrent)
+					_, err = file.Seek(int64(frameHeader.GetFrameLength()), io.SeekCurrent)
 					if err != nil {
 							fmt.Println("Error skipping message: ", err)
 							return err
